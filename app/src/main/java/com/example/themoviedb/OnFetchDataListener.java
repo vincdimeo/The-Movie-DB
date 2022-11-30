8c1fe61c721b352bfed9cd815d0ac58361859f30
franc.demarco <59506453+KekkoDM@users.noreply.github.com>
2022-11-30 17:21:05 +0100

    Sto pruvann retrofit

---
+++ app/src/main/java/com/example/themoviedb/OnFetchDataListener.java
@@ -0,0 +1,7 @@
+package com.example.themoviedb;
+import java.util.List;
+
+public interface OnFetchDataListener<NewsApiResponse> {
+    void onFetchData(List<NewsHeadlines> list, String message);
+    void onError(String message);
+}
