08ddfdfde8c33923579055d20ba1a5b7e9ce41ad
VincDM <113534568+VincDM@users.noreply.github.com>
2022-11-30 16:42:30 +0100

    First commit

---
+++ app/src/main/java/com/example/themoviedb/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.themoviedb;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.themoviedb.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
